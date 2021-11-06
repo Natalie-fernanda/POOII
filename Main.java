b648949996eb251a0e4736372a75ebc8a696a0fc
Natalie-fernanda <71447880+Natalie-fernanda@users.noreply.github.com>
2021-11-06 15:30:40 -0300

    Atividade 8
    Código da atividade 8 ainda não refatorado.

--- Main.java
+++ Main.java
@@ -1,44 +1,77 @@
-import java.time.LocalDate; 
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
+import java.util.Random;
 import java.util.Scanner;
-import java.text.ParseException;
+import java.util.Set;
+import java.util.TreeSet;
 
-class Main{
-	public static void main (String [] args) throws ParseException {
+class Main {
+	public static void main(String[] args) {
+		Random ger;
+		int seed;
+		int qtdeMax;
+		int limiteValor;
+		Scanner sc;		
+		Set<Integer> conj1;
+		Set<Integer> conj2;
+		Set<Integer> uniao;
+		Set<Integer> intersecao;
+		Set<Integer> diferenca;
 
-		try {
-			DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-			Scanner s= new Scanner(System.in);
-			String dI= s.nextLine();
-			s.close();
-			Scanner s2= new Scanner(System.in);
-			String dF= s2.nextLine();
-			s2.close();
-
-			LocalDate data1 = LocalDate.parse(dI, formato); 
-			LocalDate data2 = LocalDate.parse(dF, formato); 
-
-			Long range = java.time.temporal.ChronoUnit.DAYS.between(data1, data2);
-
-			if(range<0) {
-				int aux = range.intValue();
-				int res = aux * (-1);
-				System.out.println(res);
-			}else {
-				System.out.println(range);			
-			}
-
-
-
-		}catch(DateTimeParseException ex) {
-			System.out.println("Entrada invalida: "+ ex.getParsedString());
+		sc = new Scanner(System.in);
+		seed = sc.nextInt();
+		limiteValor = sc.nextInt();
+		qtdeMax = sc.nextInt();
+		ger = new Random(seed);
+		sc.close();
+		
+		conj1 = new TreeSet<>();
+		for (int i=0; i<qtdeMax; ++i) {
+			conj1.add(ger.nextInt(limiteValor));
+		}
+		
+		System.out.print("Conjunto 1:");
+		for (Integer elem1 : conj1) 
+			System.out.print(" " + elem1);
+		System.out.println();
+		
+		conj2 = new TreeSet<>();
+		for (int i=0; i<qtdeMax; ++i) {
+			conj2.add(ger.nextInt(limiteValor));
 		}
 
-
-
-
+		System.out.print("Conjunto 2:");
+		for (Integer elem2 : conj2) 
+			System.out.print(" " + elem2);
+		System.out.println();
+		
+		uniao = new TreeSet<>(conj1);
+		uniao.addAll(conj2);
+		
+		System.out.print("Uniao:");
+		for (Integer elem3 : uniao) 
+			System.out.print(" " + elem3);
+		System.out.println();
+		
+		intersecao = new TreeSet<>(conj1);
+		intersecao.retainAll(conj2);
+		
+		System.out.print("Intersecao:");
+		for (Integer elem4 : intersecao) 
+			System.out.print(" " + elem4);
+		System.out.println();
+		
+		diferenca = new TreeSet<>(conj2);
+		diferenca.removeAll(conj1);
+		
+		System.out.print("Diferenca 2-1:");
+		for (Integer elem5 : diferenca) 
+			System.out.print(" " + elem5);
+		System.out.println();
+		
+		diferenca = new TreeSet<>(conj1);
+		diferenca.removeAll(conj2);
+		System.out.print("Diferenca 1-2:");
+		for (Integer elem6 : diferenca) 
+			System.out.print(" " + elem6);
+		System.out.println();
 	}
-
 }
